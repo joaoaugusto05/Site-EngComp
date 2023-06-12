f45b29ff86918144d5ac08b7452868515bf116e5
Daniel-Umeda <daniel.umeda@gmail.com>
2023-06-12 15:46:48 -0300

    header notas
    header da página Notas com texto genérico

---
+++ src/pages/Notas/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Container, Wrapper, HeaderNotas, HeaderGradient, HeaderNotasTitle} from './styles';
+
+import NavBar from "../../components/NavBar";
+import Header from "../../components/Header";
+import WhatIs from "../../components/WhatIs";
+import Extras from "../../components/Extra";
+import Footer from "../../components/Footer";
+import Background from "../../assets/img/background_notas.jpg";
+import {Defaultmargin, StandardText} from "../Default/styles";
+
+const Notas = ()=>{
+    return(
+       <Container>
+          <NavBar/>
+          <Wrapper>
+            <HeaderNotas style = {{backgroundImage: `url(${Background})`}}>
+               <HeaderGradient>
+                  <Defaultmargin>
+                     <HeaderNotasTitle>
+                        Notas de Corte
+                     </HeaderNotasTitle>
+                     <StandardText style = {{color: 'white', backgroundColor: '#000e05a1'}}>
+                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!
+                     </StandardText>
+                  </Defaultmargin>
+               </HeaderGradient>
+            </HeaderNotas>
+            <WhatIs />
+            <Extras 
+            title = "EXTRACURRICULARES" 
+            text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nam recusandae velit sapiente perferendis repudiandae eum provident aut! Nobis quas magnam reprehenderit accusantium dolorum quia eos necessitatibus fugit non fuga!"
+            />
+            <Footer />
+          </Wrapper>
+       </Container>
+    )
+}
+
+export default Notas;
---
+++ src/pages/Notas/styles.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import dark from "../../themes/dark"
+
+export const Container = styled.div`
+`;
+
+export const Wrapper = styled.div`
+    //height:100vh;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    scroll-behavior: smooth;
+`;
+
+export const HeaderNotas = styled.div`
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    position:relative;
+    scroll-snap-align: center;
+    margin: 0;
+    height: 100vh;
+    width: 100vw;
+`;
+
+export const HeaderGradient = styled.div`
+    background-image: linear-gradient(to right,rgb(0, 34, 40) 20%, transparent);
+    width: 100%;
+    height: 100%;
+`;
+
+export const HeaderNotasTitle = styled.div`
+    font-family: "Ubuntu";
+    font-size: 5rem;
+    color: white;
+    display: flex;
+    padding-top: 40vh;
+    margin-bottom: 2rem;
+`;
